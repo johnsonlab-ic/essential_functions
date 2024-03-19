1ebaebf189dabe4637dc6b94452a64ad3b1c542d
ah3918 <ah3918@ic.ac.uk>
2024-03-19 15:58:41 +0000

    added LD query as a function

--- gene_expression_related/essential_functions.r
+++ gene_expression_related/essential_functions.r
@@ -161,3 +161,17 @@ chrpos_to_rsid=function(chromlocs,build=c("hg37","hg38")){
 
   return(chromlocs$rsid)
 }
+
+
+get_LD_table=function(SNP,r2){
+    
+    LD_table=ensemblQueryR::ensemblQueryLDwithSNPwindow(rsid= SNP, 
+                      r2= r2, 
+                      d.prime=0.0, 
+                      window.size=500, 
+                      pop="1000GENOMES:phase_3:EUR")
+    LD_table=LD_table[complete.cases(LD_table),]
+    LD_table=data.frame(rsid=c(SNP,LD_table$snp_in_ld),r2=c("1.000000",LD_table$r2))
+    LD_table=LD_table[complete.cases(LD_table),]
+    return(LD_table)
+}
